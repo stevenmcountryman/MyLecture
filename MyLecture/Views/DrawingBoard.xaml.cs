dcdcb2d98610365a23f1566bfdf93c0882ca5178
scountry <scountry@ITS-SCOUNTRY-W1>
2016-06-20 10:05:57 -0400

    Add project files.

---
+++ MyLecture/Views/DrawingBoard.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MyLecture.Views
+{
+    /// <summary>
+    /// This is the main drawing board class. This will be the main UI for the big screen view
+    /// </summary>
+    public sealed partial class DrawingBoard : Page
+    {
+        private Boolean isCanvasWhite = true;
+        private SolidColorBrush whiteColor = new SolidColorBrush(Colors.White);
+        private SolidColorBrush blackColor = new SolidColorBrush(Colors.Black);
+
+        public DrawingBoard()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BoardColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.invertUIColors();
+        }
+
+        private void invertUIColors()
+        {
+            isCanvasWhite = !isCanvasWhite;
+
+            if (isCanvasWhite)
+            {
+                this.MainPanel.Background = this.whiteColor;
+                this.SlideViewButton.Foreground = this.BoardColorButton.Background = this.blackColor;
+            }
+            else
+            {
+                this.MainPanel.Background = this.blackColor;
+                this.SlideViewButton.Foreground = this.BoardColorButton.Background = this.whiteColor;
+            }
+        }
+    }
+}
