f2e30e2327eb3eec227a9bc6687004280e0ef501
scountry <scountry@ITS-SCOUNTRY-W1>
2016-06-21 16:49:51 -0400

    Added "Calligraphy" custom pen. Transcribed from example custome pen on Microsoft's website written in C++. Will use this as basis for future tools and pens.

---
+++ MyLecture/Controls/CalligraphicPen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MyLecture.Controls
+{
+    public class CalligraphicPen : InkToolbarCustomPen
+    {
+        protected override InkDrawingAttributes CreateInkDrawingAttributesCore(Brush brush, double strokeWidth)
+        {
+            var attributes = new InkDrawingAttributes()
+            {
+                PenTip = PenTipShape.Circle,
+                Size = new Windows.Foundation.Size(2, 30),
+                Color = Colors.Gray,
+                PenTipTransform = Matrix3x2.CreateRotation(45.0F),
+                IgnorePressure = false,
+                DrawAsHighlighter = false,
+                FitToCurve = false
+            };
+            return attributes;
+        }
+    }
+}
